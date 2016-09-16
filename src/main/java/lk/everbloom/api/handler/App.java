242f87f63a34e4a9a7ebc84844047550e1ef3e43
Vanjikumaran <vanjikumaran@gmail.com>
2016-09-15 23:03:18 -0700

    mvn project

---
+++ src/main/java/lk/everbloom/api/handler/App.java
@@ -0,0 +1,13 @@
+package lk.everbloom.api.handler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
